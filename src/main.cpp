8233c6c9a32a5b9fe07224c85e295c0e7779b607
Vishal Kashyap <vishy789@live.com>
2020-01-31 02:12:06 +0530

    Used Fixed Function Pipeline to render a white triangle

--- src/main.cpp
+++ src/main.cpp
@@ -1,37 +1,63 @@
-#include <GLFW/glfw3.h>
+#include <glew.h>
+#include <glfw3.h>
+#include <iostream>
+
+
+
+static void error_callback(int error, const char* description)
+{
+	fprintf(stderr, "Error: %s\n", description);
+}
+
+static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, GLFW_TRUE);
+}
 
 int main(void)
 {
 	GLFWwindow* window;
 
-	/* Initialize the library */
-	if (!glfwInit())
-		return -1;
+	glfwSetErrorCallback(error_callback);
 
-	/* Create a windowed mode window and its OpenGL context */
-	window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+	if (!glfwInit())
+		exit(EXIT_FAILURE);
+
+	window = glfwCreateWindow(640, 480, "OpenGL Viewer", NULL, NULL);
 	if (!window)
 	{
 		glfwTerminate();
-		return -1;
+		exit(EXIT_FAILURE);
 	}
 
-	/* Make the window's context current */
-	glfwMakeContextCurrent(window);
+	glfwSetKeyCallback(window, key_callback);
 
-	/* Loop until the user closes the window */
+	glfwMakeContextCurrent(window);
+	glfwSwapInterval(1);
+
+	if (glewInit() != GLEW_OK)
+		std::cout << "GLEW Initialization Failed!" << std::endl;
+
+	std::cout << glGetString(GL_VERSION) << std::endl;
+
+
 	while (!glfwWindowShouldClose(window))
 	{
-		/* Render here */
 		glClear(GL_COLOR_BUFFER_BIT);
 
-		/* Swap front and back buffers */
-		glfwSwapBuffers(window);
+		glBegin(GL_TRIANGLES);
+		glVertex2f(-0.5F, -0.5F);
+		glVertex2f(0.0F, 0.5F);
+		glVertex2f(0.5F, -0.5F);
+		glEnd();
 
-		/* Poll for and process events */
+		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
 
+	glfwDestroyWindow(window);
+
 	glfwTerminate();
-	return 0;
+	exit(EXIT_SUCCESS);
 }
